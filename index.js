f725208144c2cebbfaed608db0620bf701b960ca
neon4on <vkam37@gmail.com>
2024-04-22 14:10:48 +0400

    Done

--- index.js
+++ index.js
@@ -10,24 +10,6 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-toastr.options = {
-  closeButton: true,
-  debug: false,
-  newestOnTop: false,
-  progressBar: false,
-  positionClass: 'toast-top-right',
-  preventDuplicates: false,
-  onclick: null,
-  showDuration: '300',
-  hideDuration: '1000',
-  timeOut: '5000',
-  extendedTimeOut: '1000',
-  showEasing: 'swing',
-  hideEasing: 'linear',
-  showMethod: 'fadeIn',
-  hideMethod: 'fadeOut',
-};
-
 let sets = {
   host: 'localhost',
   user: 'root',
